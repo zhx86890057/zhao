9b59b8bf4e941f73793d291db23e7742e8f38cfd
赵海啸 <767697444@qq.com>
2019-07-30 11:21:32 +0800

    security

--- upms-web/src/main/java/com/zhao/upms/web/UpmsApplication.java
+++ upms-web/src/main/java/com/zhao/upms/web/UpmsApplication.java
@@ -1,17 +1,12 @@
 package com.zhao.upms.web;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Created by hasee on 2019/2/12.
  */
 @SpringBootApplication
-@MapperScan("com.zhao.dao.mapper")
-@EnableTransactionManagement
 public class UpmsApplication {
     public static void main(String[] args) {
         SpringApplication.run(UpmsApplication.class, args);
