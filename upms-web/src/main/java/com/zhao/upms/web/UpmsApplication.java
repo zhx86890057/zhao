1605572734b25e645d1f059be98650bfe9d679bf
赵海啸 <767697444@qq.com>
2019-07-30 11:28:15 +0800

    delete

--- upms-web/src/main/java/com/zhao/upms/web/UpmsApplication.java
+++ upms-web/src/main/java/com/zhao/upms/web/UpmsApplication.java
@@ -1,14 +1,27 @@
 package com.zhao.upms.web;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 /**
  * Created by hasee on 2019/2/12.
  */
 @SpringBootApplication
+@MapperScan("com.zhao.dao.mapper")
+@EnableTransactionManagement
 public class UpmsApplication {
     public static void main(String[] args) {
         SpringApplication.run(UpmsApplication.class, args);
+    }
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
 }
